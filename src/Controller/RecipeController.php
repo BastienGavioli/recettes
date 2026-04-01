1045f58b72fb0beceab19976ffa7c5f364ee7eaf
BastienGavioli <73829228+BastienGavioli@users.noreply.github.com>
2026-04-01 22:15:26 +0200

    add creation, edition and suppression + form

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,6 +28,7 @@ final class RecipeController extends AbstractController
     public function show(Request $request, string $slug, int $id, RecipeRepository $reposotory): Response
     {
         $recipe = $reposotory->find($id);
+
         if ($slug !== $recipe->getSlug()) {
             $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $id]);
         }
@@ -32,4 +37,48 @@ final class RecipeController extends AbstractController
             'recipe' => $recipe,
         ]);
     }
+
+     #[Route('recipes/{id}/edit', name: 'recipe.edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+     public function edit(Recipe $recipe, Request $request, EntityManagerInterface $em) {
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'The recipe was successfully edited');
+            return $this->redirectToRoute('recipe.index');
+        }
+
+        return $this->render('recipe/edit.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form
+        ]);
+     }
+
+     #[Route('recipes/create', name: 'recipe.create')]
+     public function create(Request $request, EntityManagerInterface $em) {
+        $recipe = new Recipe();
+
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($recipe);
+            $em->flush();
+            $this->addFlash('success', 'The recipe was successfully created');
+            return $this->redirectToRoute('recipe.index');
+        }
+
+        return $this->render('recipe/create.html.twig', [
+            'form' => $form
+        ]);
+     }
+
+     #[Route('recipes/{id}', name: 'recipe.delete', methods: ['DELETE'])]
+     public function remove(Recipe $recipe, EntityManagerInterface $em) {
+        $em->remove($recipe);
+        $em->flush();
+        $this->addFlash('success', 'The recipe was successfully deleted');
+        return $this->redirectToRoute('recipe.index');
+     }
 }
---
+++ src/Form/RecipeType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Recipe;
+use DateTimeImmutable;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PostSubmitEvent;
+use Symfony\Component\Form\Event\PreSubmitEvent;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+class RecipeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('content')
+            ->add('duration')
+            ->add('save', SubmitType::class, [
+                'label' => 'Envoyer'
+            ])
+            ->addEventListener(FormEvents::PRE_SUBMIT, $this->autoSlug(...))
+            ->addEventListener(FormEvents::POST_SUBMIT, $this->attachTimestamps(...))
+        ;
+    }
+
+    public function autoSlug(PreSubmitEvent $event): void {
+        $data = $event->getData();
+        if (empty($data['slug'])) {
+            $slugger = new AsciiSlugger();
+            $data['slug'] = strtolower($slugger->slug($data['title']));
+            $event->setData($data);
+        }
+    }
+
+    public function attachTimestamps(PostSubmitEvent $event): void {
+        $data = $event->getData();
+        if (is_null($data->getId())) {
+            $data->setCreatedAt(new DateTimeImmutable());
+        }
+        $data->setUpdatedAt(new DateTimeImmutable());
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipe::class,
+        ]);
+    }
+}
